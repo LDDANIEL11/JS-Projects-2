29ec67241c666712433a0f06d734e0d2929261cf
LDDANIEL11 <lddaniel8313@gmail.com>
2025-07-18 00:16:07 +0330

    changed dice pics location to their new current location in js file

--- assets/DiceGame.js
+++ assets/DiceGame.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-const diceEl = document.querySelector('.dice');
-diceEl.classList.add('hidden');
+const diceEl = document.querySelector(".dice");
+diceEl.classList.add("hidden");
 
-const current0 = document.querySelector('#current--0');
-const current1 = document.querySelector('#current--1');
+const current0 = document.querySelector("#current--0");
+const current1 = document.querySelector("#current--1");
 
-const overAll0 = document.querySelector('#score--0');
-const overAll1 = document.querySelector('#score--1');
+const overAll0 = document.querySelector("#score--0");
+const overAll1 = document.querySelector("#score--1");
 
-const player0 = document.querySelector('.player--0');
-const player1 = document.querySelector('.player--1');
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
-const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const btnNew = document.querySelector(".btn--new");
 
 current0.textContent = 0;
 current1.textContent = 0;
@@ -27,7 +27,7 @@ const scores = [0, 0];
 
 const reset = function () {
   document.querySelector(`current--${activetPlayer}`);
-  diceEl.classList.add('hidden');
+  diceEl.classList.add("hidden");
   overAll0.textContent = 0;
   overAll1.textContent = 0;
   const scores = [0, 0];
@@ -35,19 +35,19 @@ const reset = function () {
 
 const switchPlayer = function () {
   document.querySelector(`#current--${activetPlayer}`).textContent = 0;
-  player0.classList.toggle('player--active');
-  player1.classList.toggle('player--active');
+  player0.classList.toggle("player--active");
+  player1.classList.toggle("player--active");
   activetPlayer = activetPlayer === 0 ? 1 : 0;
   currentScore = 0;
 };
 
 //Roll Button
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   //Generating A Random Number For The Dice
   const dice = Math.trunc(Math.random() * 6) + 1;
   //Making The Dice Appear Each Time With The Related Image For It
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove("hidden");
+  diceEl.src = `assets/dice-${dice}.png`;
   // We Set The Rules Here , If Its Not 1 We can Move On
   if (dice !== 1) {
     currentScore += dice;
@@ -65,7 +65,7 @@ btnRoll.addEventListener('click', function () {
 });
 
 //Hold Button
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   //Adding The Current Scores Of Each Player To The OverAll Of Them
   scores[activetPlayer] += currentScore;
   document.querySelector(`#score--${activetPlayer}`).textContent =
@@ -78,4 +78,4 @@ btnHold.addEventListener('click', function () {
 });
 
 //Reset Button
-btnNew.addEventListener('click', reset);
+btnNew.addEventListener("click", reset);
